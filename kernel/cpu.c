3cbb043eebf66fd1f0575a5a2e29a9e2a6ad9c25
dopaemon <polarisdp@gmail.com>
2022-01-26 00:07:07 +0700

    Revert "msm: thermal: simple: Introduce simple MSM thermal solution [msm-4.14]"
    This reverts commit 1b08ed3a32d326c6e674aa0ef300b06fb44fa5a4.

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2357,7 +2357,6 @@ EXPORT_SYMBOL(__cpu_active_mask);
 struct cpumask __cpu_isolated_mask __read_mostly;
 EXPORT_SYMBOL(__cpu_isolated_mask);
 
-
 #if CONFIG_LITTLE_CPU_MASK
 static const unsigned long lp_cpu_bits = CONFIG_LITTLE_CPU_MASK;
 const struct cpumask *const cpu_lp_mask = to_cpumask(&lp_cpu_bits);
@@ -2373,14 +2372,6 @@ const struct cpumask *const cpu_perf_mask = to_cpumask(&perf_cpu_bits);
 const struct cpumask *const cpu_perf_mask = cpu_possible_mask;
 #endif
 EXPORT_SYMBOL(cpu_perf_mask);
-
-#if CONFIG_PRIME_CPU_MASK
-static const unsigned long perfp_cpu_bits = CONFIG_PRIME_CPU_MASK;
-const struct cpumask *const cpu_perfp_mask = to_cpumask(&perfp_cpu_bits);
-#else
-const struct cpumask *const cpu_perfp_mask = cpu_possible_mask;
-#endif
-EXPORT_SYMBOL(cpu_perfp_mask);
 
 void init_cpu_present(const struct cpumask *src)
 {
